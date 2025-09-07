f9c825df6a14a2420d0c7e20fbfff38da9a86f20
Ritika-Agrawal811 <ritikaagrawal339@gmail.com>
2025-09-08 01:08:28 +0530

    feat: add create single cheatsheet api

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-contrib/cors v1.7.6
 	github.com/gin-contrib/secure v1.1.2
 	github.com/gin-gonic/gin v1.10.1
+	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jackc/pgx/v5 v5.7.5
 	github.com/joho/godotenv v1.5.1
 	github.com/rs/zerolog v1.34.0
@@ -28,11 +29,13 @@ require (
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rogpeppe/go-internal v1.10.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
---
+++ internal/services/cheatsheets/service.go
@@ -0,0 +1,51 @@
+package cheatsheets
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/Ritika-Agrawal811/sheetdeck-backend/internal/domain/dtos"
+	"github.com/Ritika-Agrawal811/sheetdeck-backend/internal/repository"
+	"github.com/Ritika-Agrawal811/sheetdeck-backend/pkg/utils"
+)
+
+type CheatsheetsService interface {
+	CreateCheatsheet(ctx context.Context, details dtos.CreateCheatsheetRequest) error
+}
+
+type cheatsheetsService struct {
+	repo *repository.Queries
+}
+
+func NewCheatsheetsService(repo *repository.Queries) CheatsheetsService {
+	return &cheatsheetsService{
+		repo: repo,
+	}
+}
+
+func (s *cheatsheetsService) CreateCheatsheet(ctx context.Context, details dtos.CreateCheatsheetRequest) error {
+
+	if details.Slug == "" || details.Title == "" {
+		return errors.New("slug and title are required")
+	}
+
+	if details.Category == "" || details.SubCategory == "" {
+		return errors.New("category and subcategory are required")
+	}
+
+	cheatsheetDetails := repository.CreateCheatsheetParams{
+		Slug:        details.Slug,
+		Title:       details.Title,
+		Category:    repository.Category(details.Category),
+		Subcategory: repository.Subcategory(details.SubCategory),
+		ImageUrl:    utils.PgText(details.ImageURL),
+	}
+
+	// Call the repository method to create a cheatsheet
+	if err := s.repo.CreateCheatsheet(ctx, cheatsheetDetails); err != nil {
+		return fmt.Errorf("failed to create cheatsheet: %w", err)
+	}
+
+	return nil
+}
