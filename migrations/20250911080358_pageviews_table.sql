5a4a499e1024b13d246934cd26c3c42412f80daa
Ritika-Agrawal811 <ritikaagrawal339@gmail.com>
2025-09-11 17:26:54 +0530

    feat: add pageviews table

---
+++ migrations/20250911080358_pageviews_table.sql
@@ -0,0 +1,31 @@
+-- +goose Up
+
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS pageviews (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    routes TEXT NOT NULL,
+    ip_address TEXT NOT NULL,
+    user_agent TEXT NOT NULL,
+    referrer TEXT,
+    viewed_at TIMESTAMP DEFAULT NOW()
+);
+
+-- Add indexes for faster filtering
+CREATE INDEX idx_pageviews_routes ON pageviews(routes);
+CREATE INDEX idx_pageviews_ip ON pageviews(ip_address);
+CREATE INDEX idx_pageviews_viewed_at ON pageviews(viewed_at);
+CREATE INDEX idx_pageviews_referrer ON pageviews(referrer);
+CREATE INDEX idx_pageviews_route_date ON pageviews(routes, viewed_at);
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX IF EXISTS idx_pageviews_referrer;
+DROP INDEX IF EXISTS idx_pageviews_viewed_at;
+DROP INDEX IF EXISTS idx_pageviews_ip;
+DROP INDEX IF EXISTS idx_pageviews_routes;
+DROP INDEX IF EXISTS idx_pageviews_route_date;
+
+DROP TABLE IF EXISTS pageviews;
+-- +goose StatementEnd
